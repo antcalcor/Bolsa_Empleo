7a96655ab2380b6e7e76dc726e57a03e4362161e
Antonio <carde46@hotmail.com>
2018-12-27 18:48:36 +0100

    añadiendo clases para hibernate

--- src/bolsa_empleo/Main.java
+++ src/bolsa_empleo/Main.java
@@ -1,10 +1,22 @@
 package bolsa_empleo;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
 
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+            .createEntityManagerFactory("bolsa_empleo");
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		
+		
+		
+		
+		
+		ENTITY_MANAGER_FACTORY.close();
 	}
-
 }
